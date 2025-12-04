7ad3d144f425125f76a2d3ff9bbc1eae3a4fe1f2
valerie-wong <74160103+valerie-wong@users.noreply.github.com>
2025-12-03 19:36:23 -0500

    fixed issues with firebase, we need to work on server.ts still

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -1,11 +1,18 @@
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import serviceAccount from './service_account.json';
 import type { ServiceAccount } from 'firebase-admin/app';
 
-const app = initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
-});
-const db = getFirestore();
+let db: Firestore;
+try {
+  const app = initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+  });
+  db = getFirestore(app);
+  console.log('Firebase Admin initialized ');
+} catch (error) {
+  console.error('Error initializing Firebase Admin:', error);
+  throw error;
+}
 
 export { db };
